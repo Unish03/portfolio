fd592f2e8056ffe1a9b0b4b7f3f92faaf1923138
Unish03 <vasulatha9843@gmail.com>
2025-08-08 16:48:43 +0530

    react project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1 class="animated-heading">Unish Kumar V <a href="./resume.pdf" target="_blank" rel="noopener" title="Download Resume">🚀</a></h1>
+        <div class="blowing-text">
+          Driven By Passion, Driven By Curiosity, Powered By Code
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
